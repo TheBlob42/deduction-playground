487b1a43fad1ef83fced062f6f357ede858fbb90
TheBlob42 <hessenmobbel@web.de>
2015-09-01 12:10:25 +0200

    cleaned up repl

--- src/deduction_playground/repl.clj
+++ src/deduction_playground/repl.clj
@@ -3,20 +3,23 @@
             [deduction-playground.read-rules :as read]
             [deduction-playground.printer :refer [pprint]]))
 
-(read/read-rules "resources/rules-temporal.clj")
-(read/read-rules "resources/classical-theorems.clj")
 ;(read/read-rules "resources/rules.clj")
-(read/read-theorems "resources/theorems.clj")
+(read/read-rules "resources/rules-temporal.clj")
 (read/read-classicals "resources/classical-theorems.clj")
+(read/read-theorems "resources/theorems.clj")
 
-(def last_step (atom []))
+;; holds the actual state of the proof 
 (def p (atom []))
+;; holds the second actual state of the proof for undo steps
+(def last_step (atom []))
 
 (defn show
+  "Print the actual state of the proof"
   []
   (pprint @p))
 
 (defn proof
+  "Start a new proof"
   ([formula] (proof [] formula))
   ([premises formula]
     (reset! last_step [])
@@ -24,45 +27,49 @@
     (show)))
 
 (defn step-f
+  "Execute a forward step"
   [rule & lines]
   (reset! last_step @p)
   (swap! p #(apply proof/step-f (conj (conj lines rule) %)))
   (show))
 
-;; TEST
 (defn step-f-inside
-  [rule & lines]
+  "Executs a forward step inside the chosen line"
+  [rule line]
   (reset! last_step @p)
-  (swap! p #(apply proof/step-f-inside (conj lines rule %)))
+  (swap! p #(apply proof/step-f-inside (conj line rule %)))
   (show))
-;; TEST END
 
 (defn step-b
+  "Execute a backward step"
   [rule & lines]
   (reset! last_step @p)
   (swap! p #(apply proof/step-b (conj (conj lines rule) %)))
   (show))
 
 (defn choose-option
+  "Chose an option in the chosen line"
   [line num]
   (reset! last_step @p)
   (swap! p proof/choose-option line num)
   (show))
 
 (defn rename-var
+  "Rename a variable"
   [old new]
   (reset! last_step @p)
   (swap! p proof/rename-var old new)
   (show))
 
 (defn classical
+  "Apply the classical-theorems inside the chosen line"
   [line]
   (reset! last_step @p)
   (swap! p proof/classical line)
   (show))
 
-
 (defn export-theorem
+  "Export the solved proof to a file as a theorem"
   [filename name]
   (read/export-theorem 
     @p
@@ -70,17 +77,12 @@
     name))
 
 (defn undo []
+  "Undo the last change (you can't go further than the last state)"
   (if (empty? @last_step)
     (println "There is nothing to undo (you can only return to the last state not further)")
     (do
       (reset! p @last_step)
       (reset! last_step [])
       (show))))
-
-
-;; STUFF FOR DEBUGGING
-
-  
-  
-  
+ 
   
