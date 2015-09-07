fdaf19ef52498a343420e9763031d5341463b500
TheBlob42 <hessenmobbel@web.de>
2015-09-07 16:44:44 +0200

    added undo steps up to the beginning of the proof

--- src/deduction_playground/repl.clj
+++ src/deduction_playground/repl.clj
@@ -10,8 +10,8 @@
 
 ;; holds the actual state of the proof 
 (def p (atom []))
-;; holds the second actual state of the proof for undo steps
-(def last_step (atom []))
+;; holds the last steps since the beginning of the proof (for undo steps)
+(def last_steps (atom []))
 
 (defn show
   "Print the actual state of the proof"
@@ -22,49 +22,49 @@
   "Start a new proof"
   ([formula] (proof [] formula))
   ([premises formula]
-    (reset! last_step [])
+    (reset! last_steps [])
     (reset! p (deduc/proof premises formula))
     (show)))
 
 (defn step-f
   "Execute a forward step"
   [rule & lines]
-  (reset! last_step @p)
+  (swap! last_steps conj @p)
   (swap! p #(apply deduc/step-f (conj (conj lines rule) %)))
   (show))
 
 (defn step-f-inside
   "Executs a forward step inside the chosen line"
   [rule line]
-  (reset! last_step @p)
+  (swap! last_steps conj @p)
   (swap! p #(apply deduc/step-f-inside (conj line rule %)))
   (show))
 
 (defn step-b
   "Execute a backward step"
   [rule & lines]
-  (reset! last_step @p)
+  (swap! last_steps conj @p)
   (swap! p #(apply deduc/step-b (conj (conj lines rule) %)))
   (show))
 
 (defn choose-option
   "Choose an option in the chosen line"
   [line num]
-  (reset! last_step @p)
+  (swap! last_steps conj @p)
   (swap! p deduc/choose-option line num)
   (show))
 
 (defn rename-var
   "Rename a variable"
   [old new]
-  (reset! last_step @p)
+  (swap! last_steps conj @p)
   (swap! p deduc/rename-var old new)
   (show))
 
 (defn classical
   "Apply the classical-theorems inside the chosen line"
   [line]
-  (reset! last_step @p)
+  (swap! last_steps conj @p)
   (swap! p deduc/classical line)
   (show))
 
@@ -78,11 +78,11 @@
 
 (defn undo []
   "Undo the last change (you can't go further than the last state)"
-  (if (empty? @last_step)
-    (println "There is nothing to undo (you can only return to the last state not further)")
+  (if (empty? @last_steps)
+    (println "You reached the starting point, there is nothing more to undo")
     (do
-      (reset! p @last_step)
-      (reset! last_step [])
+      (reset! p (last @last_steps))
+      (swap! last_steps #(into [] (drop-last %)))
       (show))))
  
   
